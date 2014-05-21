1223b66a7daa38770ad8c2bbe6dff67220d4109c
Jimmy Feterman <jimmyfeterman2015@u.northwestern.edu>
2014-05-21 16:51:04 -0500

    Team4 Edits May 21th 2014

---
+++ team 4 script.js
@@ -0,0 +1,35 @@
+$(".home").click(backToTop); 
+console.log("hi");
+
+function backToTop(){
+window.scrollTo(5,635);
+} 
+
+$("#boxA").click(scrollGoodRelationships);
+console.log("Yo");
+
+function scrollGoodRelationships(){
+	window.scrollTo(5,1370);
+}
+
+$("#boxB").click(scrollBadRelationships);
+console.log("Aye");
+
+function scrollBadRelationships(){
+	window.scrollTo(5,2254);
+}
+
+$("#boxC").click(scrollWorstCase);
+console.log("Hey");
+
+function scrollWorstCase(){
+	window.scrollTo(5,3166);
+}
+
+
+$("#boxD").click(scrollLandlord);
+console.log("Hello");
+
+function scrollLandlord(){
+	window.scrollTo(5,4461);
+}
